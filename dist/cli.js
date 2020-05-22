dc1d1e659e4e32b52d2bb2bc892139f72bc4a59f
Oded Sharon <odedshr@gmail.com>
2020-05-22 21:57:09 +0100

    updated cli

--- dist/cli.js
+++ dist/cli.js
@@ -10,6 +10,13 @@ var __assign = (this && this.__assign) || function () {
     };
     return __assign.apply(this, arguments);
 };
+var __spreadArrays = (this && this.__spreadArrays) || function () {
+    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+    for (var r = Array(s), k = 0, i = 0; i < il; i++)
+        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
+            r[k] = a[j];
+    return r;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var path_1 = require("path");
 var fs_1 = require("fs");
@@ -21,25 +28,43 @@ var args = process.argv;
 var ts = args.indexOf('ts') > -1;
 var ssr = args.indexOf('ssr') > -1;
 entries.map(function (entry) {
-    var targets = prepareTargets(entry.target, ts, ssr);
+    var targets = standarizeTargets(entry.target || [{ ts: ts, ssr: ssr }], ts, ssr);
     return toArray(entry.source).map(function (source) {
         return chokidar_1.watch(source)
-            .on('add', function (path) { return copy(source, path, targets || [{ path: replaceFileExtension(path, ts), ts: ts, ssr: ssr }]); })
-            .on('change', function (path) { return copy(source, path, targets || [{ path: replaceFileExtension(path, ts), ts: ts, ssr: ssr }]); })
-            .on('unlink', function (path) { return remove(source, path, targets || [{ path: replaceFileExtension(path, ts), ts: ts, ssr: ssr }]); })
-            .on('addDir', function (path) { return source !== path && copy(source, path, targets || [{ path: replaceFileExtension(path, ts), ts: ts, ssr: ssr }]); })
-            .on('unlinkDir', function (path) { return remove(source, path, targets || [{ path: replaceFileExtension(path, ts), ts: ts, ssr: ssr }]); });
+            .on('add', function (path) { return copy(path, getTargets(targets, path)); })
+            .on('change', function (path) { return copy(path, getTargets(targets, path)); })
+            .on('unlink', function (path) { return remove(source, path, getTargets(targets, path)); })
+            .on('addDir', function (path) { return source !== path && copy(path, getTargets(targets, path)); })
+            .on('unlinkDir', function (path) { return remove(source, path, getTargets(targets, path)); });
     });
 });
-function prepareTargets(targets, ts, ssr) {
+function standarizeTargets(targets, ts, ssr) {
     return toArray(targets).map(function (target) {
         return typeof target === 'string' ? { path: target, ts: ts, ssr: ssr } : __assign({ ts: ts, ssr: ssr }, target);
     });
+}
+function getTargets(targets, sourcePath) {
+    return targets.map(function (target) {
+        var path = replaceFileExtension(sourcePath, target.ts);
+        if (target.path !== undefined) {
+            path = (isTargetFolder(target.path))
+                ? __spreadArrays(target.path.split('/'), [path.split('/').pop()]).join('/').replace('//', '/')
+                : target.path;
+        }
+        return { path: path, ts: target.ts, ssr: target.ssr };
+    });
+}
+// return true if path doesn't end with '/', is an existing folder or last element has no '.' in it
+function isTargetFolder(path) {
+    return path.charAt(path.length - 1) === '/' ||
+        (fs_1.existsSync(path) && fs_1.lstatSync(path).isDirectory()) ||
+        path.split('/').pop().indexOf('.') === -1;
 }
 function toArray(element) {
     return Array.isArray(element) ? element : [element];
 }
 function getTargetPath(source, file, target) {
+    console.log('\n\ngetTargetPath', source, target);
     return file.replace(source, target);
 }
 function remove(source, file, targets) {
@@ -47,9 +72,9 @@ function remove(source, file, targets) {
         return removeFileSync(getTargetPath(source, replaceFileExtension(file, target.ts), target.path));
     });
 }
-function copy(source, file, targets) {
+function copy(file, targets) {
     targets.forEach(function (target) {
-        return console.log(copyFolderRecursiveSync(file, __assign(__assign({}, target), { path: getTargetPath(source, replaceFileExtension(file, target.ts), target.path) })));
+        return console.log(copyFolderRecursiveSync(file, target));
     });
 }
 function replaceFileExtension(filename, toTypescript) {
@@ -96,7 +121,7 @@ function copyFolderRecursiveSync(source, target) {
     else {
         copyFileSync(source, target);
     }
-    return "copying " + source + " => " + target;
+    return "copying " + source + " => " + target.path;
 }
 function removeFileSync(path) {
     if (fs_1.existsSync(path)) {
